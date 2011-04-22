a000ba037d274826056eaf812f8789b3e070c184
Alfredo Ribeiro <alfredo@aatecnologia.com.br>
2011-04-22 15:52:30 -0300

    add setting table and dashboard index

---
+++ app/models/setting.rb
@@ -0,0 +1,31 @@
+class Setting
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  field :admin_locale           ,:type => String, :default => "en"
+  field :front_locale           ,:type => String, :default => "en"
+  field :multi_language         ,:type => Boolean, :default => false
+  
+  field :website_title          ,:type => String
+  field :website_description    ,:type => String
+  field :website_keywords       ,:type => String
+  field :website_facebook       ,:type => String
+  field :website_twitter        ,:type => String
+  field :website_orkut          ,:type => String
+  field :website_linkedin       ,:type => String
+
+  field :company_country        ,:type => String
+  field :company_city           ,:type => String
+  field :company_zipcode        ,:type => String
+  field :company_neiborhood     ,:type => String
+  field :company_street         ,:type => String
+  field :company_address_number ,:type => String
+  field :company_address_more   ,:type => String
+
+  field :contact_notification_email ,:type => String
+  field :contact_website_email      ,:type => String
+  field :contact_form_email         ,:type => String
+
+  field :google_analytics_code      ,:type => String
+  field :google_analytics_custom    ,:type => String
+end
