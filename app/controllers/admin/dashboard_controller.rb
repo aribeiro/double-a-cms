f9a5d9e233435516ccca1183bf46dd0a14d1920a
Alfredo Ribeiro <alfredo@aatecnologia.com.br>
2011-04-25 12:42:48 -0300

    add activities log show to dashboard index

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardController < Admin::ApplicationController
   def index
+    @activities = ActivityLog::Activity.desc(:created_at).all
   end
-
 end
--- spec/controllers/admin/dashboard_controller_spec.rb
+++ spec/controllers/admin/dashboard_controller_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
 
 describe Admin::DashboardController do
-
+  login_user
+  
+  def mock_activity(stubs={})
+    @mock_activity ||= mock_model(ActivityLog::Activity, stubs).as_null_object
+  end
+  
   describe "GET 'index'" do
-    it "should be successful" do
-      get 'index'
+    it "be successful" do
+      get :index
       response.should be_success
+    end
+
+    it "assings all ActivityLog Activities as @activities" do
+      ActivityLog::Activity.stub_chain(:desc, :all) {[mock_activity]}
+      get :index
+      assigns(:activities).should == [mock_activity]
     end
   end
 
