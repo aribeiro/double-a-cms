e5998c4ef8fca9bd080f6f29bae2f895293122d8
Alfredo Ribeiro <alfredo@aatecnologia.com.br>
2011-04-25 12:41:41 -0300

    add activity log to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
+  include ActivityLog
   
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
